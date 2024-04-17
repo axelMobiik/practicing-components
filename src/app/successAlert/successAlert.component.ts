9a5351ebe0027d6cf04c30cc2eafe4ef4ec5ab8a
Axel Coronado Zepeda <axel.coronado@mobiik.intranet>
2024-04-17 16:38:27 -0700

    first commit

---
+++ src/app/successAlert/successAlert.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-success-alert',
+  templateUrl: './successAlert.component.html',
+  styleUrl: './successAlert.component.css',
+})
+export class SuccessAlertComponent {}
---
+++ src/app/warningAlert/warningAlert.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-warning-alert',
+  templateUrl: './warningAlert.component.html',
+  styleUrl: './warningAlert.component.css',
+})
+export class WarningAlertComponent {}
